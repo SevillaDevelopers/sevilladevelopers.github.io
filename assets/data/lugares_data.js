a6eaad228c9729c71ad8cb9360007568f34f9183
Luis Mesa <luismesalas@gmail.com>
2019-04-14 20:26:31 +0200

    Created new pins in order to display event hosts. Included updated list of hosts.

--- assets/data/lugares_data.js
+++ assets/data/lugares_data.js
@@ -1,4 +1,4 @@
-lugares =
+lugares_trabajo =
     [
         {
             "name": "Cafetería Hotel NH Plaza de Armas",
@@ -65,3 +65,72 @@ lugares =
             "recommended_by": "Wimel (@wimel85)"
         }
     ];
+
+lugares_eventos =
+    [
+        {
+            "name": "UOC Sevilla",
+            "latlong": [37.40044,-5.99828],
+            "email": "uocsevilla@uoc.edu"
+        },
+        {
+            "name": "Bitnami",
+            "latlong": [37.37693,-6.00334],
+            "email": "info@bitnami.com"
+        },
+        {
+            "name": "workINcompany",
+            "latlong": [37.3908674, -5.9956827],
+            "email": "info@workincompany.com"
+        },
+		{
+			"name": "El Cubo (Andalucía Open Future)",
+			"latlong": [37.40822,-6.00316],
+			"email": "andalucia.cubo@openfuture.org"
+		},
+		{
+			"name": "ETSII Universidad de Sevilla",
+			"latlong": [37.35820,-5.98695],
+			"email": "ptrinidad@us.es"
+		},
+		{
+			"name": "Espacio RES",
+			"latlong": [37.3776392, -6.0019989],
+			"email": "hola@espaciores.org"
+		},
+		{
+			"name": "Radiópolis",
+			"latlong": [37.39684,-6.00237],
+			"email": "juntadirectiva@radiopolis.org"
+		},
+		{
+			"name": "La Casa Ensamblá",
+			"latlong": [37.3972619, -5.9891158],
+			"email": "lacasaensambla@gmail.com"
+		},
+		{
+			"name": "FESAC",
+			"latlong": [37.37166,-5.99924],
+			"email": "carlos.ojeda@fesac.es"
+		},
+		{
+			"name": "Tramallol",
+			"latlong": [37.3978203, -5.9852294],
+			"email": "maki.yedra@gmail.com"
+		},
+		{
+			"name": "GoHub (Acuario de Sevilla)",
+			"latlong": [37.37049,-5.99088],
+			"email": "maria.gil@gohub.tech"
+		},
+		{
+            "name": "Escuela de Organización Industrial",
+            "latlong": [37.40456,-6.00673],
+            "email": "infoandalucia@eoi.es"
+        },
+        {
+            "name": "Sngular Hub Sevilla",
+            "latlong": [37.3803481,-5.9739242],
+            "email": "marta.segura@sngular.com"
+        }
+	];
